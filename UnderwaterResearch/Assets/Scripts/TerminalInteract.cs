954e0a2076dd37d0a72aabcb07439861bc29dca5
Brandon Gros <grosbrandon04@gmail.com>
2026-04-08 19:19:36 -0500

    Added terminal UI code pad and interaction scripts

---
+++ UnderwaterResearch/Assets/Scripts/TerminalInteract.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TerminalInteract : MonoBehaviour
+{
+  [SerializeField] private GameObject terminalUI;
+
+  void Start()
+  {
+    if (terminalUI != null)
+      terminalUI.SetActive(false);
+  }
+
+  void Grab()
+  {
+    if (terminalUI == null) return;
+    terminalUI.SetActive(true);
+    Cursor.visible = true;
+    Cursor.lockState = CursorLockMode.None;
+    Debug.Log("Terminal opened!");
+  }
+}
